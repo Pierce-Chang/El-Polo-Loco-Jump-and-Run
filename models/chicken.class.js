54eee6d06d69f4fe27fb0e6418b461da38a85d98
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-10-16 21:07:51 +0200

    addet enemie walk animation

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -4,11 +4,27 @@ class Chicken extends MoveableObject {
     height = 80;
     width = 80;
 
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+    ];
+    currentImage = 0;
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
-
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
         this.x = 200 + Math.random() * 500;
     }
 
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100); // Interval every 1000ms
+    }
 
 }
