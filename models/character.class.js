5c007c444f89f779748e6e9191b705e49fead835
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-10-21 13:53:16 +0200

    added walking speed and horizontal movement

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MoveableObject {
 
     height = 200;
     y = 225;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -10,6 +11,7 @@ class Character extends MoveableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png',
     ];
+    world;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -19,12 +21,27 @@ class Character extends MoveableObject {
     }
 
     animate() {
+
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }, 100); // Interval every 1000ms
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT) {
+                // Walk animation
+                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6;
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 50); // Interval every 1000ms
+
     }
 
     jump() {
