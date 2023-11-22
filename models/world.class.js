96c6a6b1b2021568bfcdb24435c5e66034f0f6f7
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-11-22 21:50:07 +0100

    colliding with endboss updated

--- models/world.class.js
+++ models/world.class.js
@@ -48,6 +48,11 @@ class World extends DrawableObject {
                         console.log('Endboss got hit by bottle!')
                         // Endboss was hit by the bottle
                         this.hitEndboss();
+                        const index = this.ThrowableObjects.indexOf(bottle);
+                        if (index !== -1) {
+                            this.ThrowableObjects.splice(index, 1);
+                        }
+                        clearInterval(bottleAnimationInterval); // Clear the animation interval
                     } else {
                         console.log('No collision detected with Endboss');
                     }
@@ -116,8 +121,10 @@ class World extends DrawableObject {
 
     hitEndboss() {
         // Reduziere die Endboss-Statusleiste um 34%
-        this.statusBarEndboss.setPercentage(this.statusBarEndboss.percentage - 34);
-        console.log('Endboss hit, new percentage:', this.statusBarEndboss.percentage);
+        const currentPercentage = this.statusBarEndboss.percentage;
+        const newPercentage = Math.max(0, currentPercentage - 34);
+        this.statusBarEndboss.setPercentage(newPercentage);
+        console.log('Endboss hit, new percentage:', newPercentage);
     }
 
     collectCoin() {
@@ -150,6 +157,11 @@ class World extends DrawableObject {
             const index = this.level.enemies.indexOf(object);
             if (index !== -1) {
                 this.level.enemies.splice(index, 1);
+            }
+        } else if (object instanceof ThrowableObejct) {
+            const index = this.ThrowableObjects.indexOf(object);
+            if (index !== -1) {
+                this.ThrowableObjects.splice(index, 1);
             }
         }
     }
