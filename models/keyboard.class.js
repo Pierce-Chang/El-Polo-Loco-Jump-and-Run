fc384435bbab672d36420936b4d531777cc82b38
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-11-30 20:23:32 +0100

    mobile version and responsive design

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,5 +1,9 @@
 class Keyboard {
 
+    constructor() {
+        this.bindBtsPressEvents();
+    }
+
     LEFT = false;
     RIGHT = false;
     UP = false;
@@ -10,4 +14,16 @@ class Keyboard {
 
     A = false; // Left
     D = false; // Right
+
+    bindBtsPressEvents() {
+        document.getElementById('button-up').addEventListener('touchstart', () => { if (!world.gamePaused) this.SPACE = true; });
+        document.getElementById('button-e').addEventListener('touchstart', () => { if (!world.gamePaused) this.E = true; });
+        document.getElementById('button-right').addEventListener('touchstart', () => { if (!world.gamePaused) this.RIGHT = true; });
+        document.getElementById('button-left').addEventListener('touchstart', () => { if (!world.gamePaused) this.LEFT = true; });
+        document.getElementById('button-up').addEventListener('touchend', () => { if (!world.gamePaused) this.SPACE = false; });
+        document.getElementById('button-e').addEventListener('touchend', () => { if (!world.gamePaused) this.E = false; });
+        document.getElementById('button-right').addEventListener('touchend', () => { if (!world.gamePaused) this.RIGHT = false; });
+        document.getElementById('button-left').addEventListener('touchend', () => { if (!world.gamePaused) this.LEFT = false; });
+
+    }
 }
